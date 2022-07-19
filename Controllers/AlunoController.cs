8056b5153cd3d4b5d9eb4ea80a4a36b7d98d71bd
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-07-19 17:10:25 -0300

    Add project files.

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PoCWebAPI.Models;
+
+namespace PoCWebAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        private EscolaContext dbEscola = new EscolaContext();      
+
+        [HttpGet]
+        public List<Aluno> AcessarAlunos()
+        {
+            return dbEscola.Alunos.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Aluno AcessarAlunoPelaId(int id)
+        {          
+            Aluno aluno = dbEscola.Alunos.Find(id);
+            return aluno;
+        }
+
+        [HttpPost]
+        public Aluno CadastrarAluno(Aluno novoAluno)
+        {
+            dbEscola.Alunos.Add(novoAluno);
+            dbEscola.SaveChanges();
+
+            return novoAluno;
+        }
+
+        [HttpDelete]
+        public Aluno DeleteAluno(int id)
+        {
+            Aluno aluno = dbEscola.Alunos.Find(id);
+
+            dbEscola.Alunos.Remove(aluno);
+            dbEscola.SaveChanges();
+
+            return aluno;
+        }
+
+
+
+
+
+    }
+}
